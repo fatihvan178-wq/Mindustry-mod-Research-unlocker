4c2a57dceea6c1169863eeb47e1739901a3c262c
fatihvan178-wq <fatihvan178@gmail.com>
2026-03-09 00:54:41 +0700

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,10 @@
-
+Events.on(ClientLoadEvent, e => {
+    Vars.content.each(c => {
+        if(c instanceof UnlockableContent){
+            c.alwaysUnlocked = true;
+        }
+        if(c instanceof SectorPreset){
+            c.alwaysUnlocked = true;
+        }
+    });
+});
